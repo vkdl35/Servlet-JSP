7cb03f857c593286137aa20c1b518755e0f85da8
vkdl35 <106090523+vkdl35@users.noreply.github.com>
2022-09-27 13:09:29 +0900

    Add files via upload

--- JSPPrj/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
+++ JSPPrj/src/main/java/com/newlecture/web/controller/NoticeDetailController.java
@@ -14,6 +14,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.newlecture.web.entity.Notice;
+
 @WebServlet("/notice/detail")
 public class NoticeDetailController extends HttpServlet {
 	@Override
@@ -41,12 +43,26 @@ public class NoticeDetailController extends HttpServlet {
 			String files = rs.getString("FILES");
 			String content = rs.getString("CONTENT");
 			
+			Notice notice = new Notice(
+						id,
+						title,
+						writerId,
+						regdate,
+						hit,
+						files,
+						content
+					);
+			
+			request.setAttribute("n", notice);
+			
+			/*
 			request.setAttribute("title", title);
 			request.setAttribute("writerId", writerId);
 			request.setAttribute("regdate", regdate);
 			request.setAttribute("hit", hit);
 			request.setAttribute("files", files);
 			request.setAttribute("content", content);
+			*/
 
 			rs.close();
 			st.close();
